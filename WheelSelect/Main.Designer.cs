1d7a8a277039f86cebaccc7dad3e9f206d018cf3
fischgeek <fischgeek@gmail.com>
2020-06-10 20:34:33 -0500

    initial commit

---
+++ WheelSelect/Main.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace WheelSelect
+{
+    partial class WheelSelect
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.top3 = new System.Windows.Forms.Label();
+            this.top2 = new System.Windows.Forms.Label();
+            this.top1 = new System.Windows.Forms.Label();
+            this.selected = new System.Windows.Forms.Label();
+            this.bottom1 = new System.Windows.Forms.Label();
+            this.bottom2 = new System.Windows.Forms.Label();
+            this.bottom3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // top3
+            // 
+            this.top3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.top3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.top3.ForeColor = System.Drawing.Color.Silver;
+            this.top3.Location = new System.Drawing.Point(0, 0);
+            this.top3.Margin = new System.Windows.Forms.Padding(0);
+            this.top3.Name = "top3";
+            this.top3.Size = new System.Drawing.Size(484, 20);
+            this.top3.TabIndex = 10;
+            this.top3.Text = "abcdefghijklmnopqrstuvwxyz";
+            this.top3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // top2
+            // 
+            this.top2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.top2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.top2.ForeColor = System.Drawing.Color.Gray;
+            this.top2.Location = new System.Drawing.Point(0, 20);
+            this.top2.Margin = new System.Windows.Forms.Padding(0);
+            this.top2.Name = "top2";
+            this.top2.Size = new System.Drawing.Size(484, 20);
+            this.top2.TabIndex = 9;
+            this.top2.Text = "abcdefghijklmnopqrstuvwxyz";
+            this.top2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // top1
+            // 
+            this.top1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.top1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.top1.ForeColor = System.Drawing.Color.DimGray;
+            this.top1.Location = new System.Drawing.Point(0, 40);
+            this.top1.Margin = new System.Windows.Forms.Padding(0);
+            this.top1.Name = "top1";
+            this.top1.Size = new System.Drawing.Size(484, 28);
+            this.top1.TabIndex = 8;
+            this.top1.Text = "abcdefghijklmnopqrstuvwxyz";
+            this.top1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // selected
+            // 
+            this.selected.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.selected.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.selected.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.selected.ForeColor = System.Drawing.Color.SteelBlue;
+            this.selected.Location = new System.Drawing.Point(-1, 71);
+            this.selected.Margin = new System.Windows.Forms.Padding(0, 3, 0, 3);
+            this.selected.Name = "selected";
+            this.selected.Size = new System.Drawing.Size(486, 40);
+            this.selected.TabIndex = 14;
+            this.selected.Text = "abcdefghijklmnopqrstuvwxyz";
+            this.selected.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // bottom1
+            // 
+            this.bottom1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bottom1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bottom1.ForeColor = System.Drawing.Color.DimGray;
+            this.bottom1.Location = new System.Drawing.Point(0, 114);
+            this.bottom1.Margin = new System.Windows.Forms.Padding(0);
+            this.bottom1.Name = "bottom1";
+            this.bottom1.Size = new System.Drawing.Size(484, 28);
+            this.bottom1.TabIndex = 11;
+            this.bottom1.Text = "abcdefghijklmnopqrstuvwxyz";
+            this.bottom1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // bottom2
+            // 
+            this.bottom2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bottom2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bottom2.ForeColor = System.Drawing.Color.Gray;
+            this.bottom2.Location = new System.Drawing.Point(0, 142);
+            this.bottom2.Margin = new System.Windows.Forms.Padding(0);
+            this.bottom2.Name = "bottom2";
+            this.bottom2.Size = new System.Drawing.Size(484, 20);
+            this.bottom2.TabIndex = 12;
+            this.bottom2.Text = "abcdefghijklmnopqrstuvwxyz";
+            this.bottom2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // bottom3
+            // 
+            this.bottom3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.bottom3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bottom3.ForeColor = System.Drawing.Color.Silver;
+            this.bottom3.Location = new System.Drawing.Point(0, 162);
+            this.bottom3.Margin = new System.Windows.Forms.Padding(0);
+            this.bottom3.Name = "bottom3";
+            this.bottom3.Size = new System.Drawing.Size(484, 20);
+            this.bottom3.TabIndex = 13;
+            this.bottom3.Text = "abcdefghijklmnopqrstuvwxyz";
+            this.bottom3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.selected);
+            this.panel1.Controls.Add(this.top3);
+            this.panel1.Controls.Add(this.bottom3);
+            this.panel1.Controls.Add(this.top2);
+            this.panel1.Controls.Add(this.bottom2);
+            this.panel1.Controls.Add(this.top1);
+            this.panel1.Controls.Add(this.bottom1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(486, 189);
+            this.panel1.TabIndex = 15;
+            // 
+            // WheelSelect
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(486, 189);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "WheelSelect";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label top3;
+        private System.Windows.Forms.Label top2;
+        private System.Windows.Forms.Label top1;
+        private System.Windows.Forms.Label selected;
+        private System.Windows.Forms.Label bottom1;
+        private System.Windows.Forms.Label bottom2;
+        private System.Windows.Forms.Label bottom3;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ WheelSelect/Main.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WheelSelect
+{
+    public partial class WheelSelect : Form
+    {
+        private List<string> theList = new List<string>();
+        private int selectedIndex = 0;
+        private string saveLocation = ConfigurationManager.AppSettings["SaveLocation"] ?? @"c:\wheel_selection.txt";
+        private string windowBackgroundColor = ConfigurationManager.AppSettings["WindowBackgroundColor"];
+        private string selectedTextColor = ConfigurationManager.AppSettings["SelectedTextColor"];
+        private string offset1TextColor = ConfigurationManager.AppSettings["Offset1TextColor"];
+        private string offset2TextColor = ConfigurationManager.AppSettings["Offset2TextColor"];
+        private string offset3TextColor = ConfigurationManager.AppSettings["Offset3TextColor"];
+
+        public WheelSelect()
+        {
+            InitializeComponent();
+            this.MouseWheel += Form1_MouseWheel;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TrySetColors();
+            HandleArgs();
+            HandleEvent(false);
+        }
+
+        private void HandleArgs()
+        {
+            string[] args = Environment.GetCommandLineArgs();
+            // args[0] seems to always = execution path. Neat.
+            if (args.Length == 3) {
+                var delim = args[1];
+                var data = args[2];
+                if (delim == "\\n") {
+                    delim = "\n";
+                    data = data.Replace("\\n", "\n");
+                }
+                var splitData = Regex.Split(data, delim);
+                theList.AddRange(splitData);
+                selectedIndex = -1;
+            }
+        }
+
+        private void Form1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape) {
+                Environment.Exit(0);
+            } else if (e.KeyCode == Keys.Enter) {
+                SaveSelection();
+            } else if (e.KeyCode == Keys.Up) {
+                HandleEvent(true);
+            } else if (e.KeyCode == Keys.Down) {
+                HandleEvent(false);
+            }
+        }
+
+        private void Form1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (e.Delta > 0) {
+                HandleEvent(true);
+            } else {
+                HandleEvent(false);
+            }
+        }
+
+        private void HandleEvent(bool up)
+        {
+            if (up) {
+                selectedIndex = GetPreviousIndex();
+            } else {
+                selectedIndex = GetNextIndex();
+            }
+            top3.Text = SetText(Position.Top3);
+            top2.Text = SetText(Position.Top2);
+            top1.Text = SetText(Position.Top1);
+            selected.Text = SetText(Position.Primary);
+            bottom1.Text = SetText(Position.Bottom1);
+            bottom2.Text = SetText(Position.Bottom2);
+            bottom3.Text = SetText(Position.Bottom3);
+        }
+
+        private string SetText(Position pos)
+        {
+            var trackInt = 0;
+            switch (pos) {
+                case Position.Top3:
+                    trackInt = selectedIndex - 3;
+                    break;
+                case Position.Top2:
+                    trackInt = selectedIndex - 2;
+                    break;
+                case Position.Top1:
+                    trackInt = selectedIndex - 1;
+                    break;
+                case Position.Primary:
+                    trackInt = selectedIndex;
+                    break;
+                case Position.Bottom1:
+                    trackInt = selectedIndex + 1;
+                    break;
+                case Position.Bottom2:
+                    trackInt = selectedIndex + 2;
+                    break;
+                case Position.Bottom3:
+                    trackInt = selectedIndex + 3;
+                    break;
+                default:
+                    trackInt = selectedIndex;
+                    break;
+            }
+            if (trackInt > theList.Count() - 1) {
+                return "";
+            }
+            if (trackInt < 0) {
+                return "";
+            }
+            return theList[trackInt];
+        }
+
+        private int GetPreviousIndex()
+        {
+            if (selectedIndex == 0) {
+                return 0;
+            } else if (selectedIndex == theList.Count()) {
+                return selectedIndex;
+            }
+            return selectedIndex - 1;
+        }
+
+        private int GetNextIndex()
+        {
+            if (selectedIndex == theList.Count() - 1) {
+                return selectedIndex;
+            }
+            return selectedIndex + 1;
+        }
+
+        private void SaveSelection()
+        {
+            File.AppendAllText(saveLocation, selected.Text);
+            Environment.Exit(0);
+        }
+
+        private void TrySetColors()
+        {
+            try {
+                this.BackColor = Color.FromName(windowBackgroundColor);
+            } catch (Exception) {
+                this.BackColor = Color.White;
+            }
+            try {
+                this.selected.ForeColor = Color.FromName(selectedTextColor);
+            } catch (Exception) {
+                this.selected.ForeColor = Color.SteelBlue;
+            }
+            try {
+                this.top1.ForeColor = Color.FromName(offset1TextColor);
+                this.bottom1.ForeColor = Color.FromName(offset1TextColor);
+            } catch (Exception) {
+                this.top1.ForeColor = Color.DimGray;
+                this.bottom1.ForeColor = Color.DimGray;
+            }
+            try {
+                this.top2.ForeColor = Color.FromName(offset2TextColor);
+                this.bottom2.ForeColor = Color.FromName(offset2TextColor);
+            } catch (Exception) {
+                this.top2.ForeColor = Color.Gray;
+                this.bottom2.ForeColor = Color.Gray;
+            }
+            try {
+                this.top3.ForeColor = Color.FromName(offset3TextColor);
+                this.bottom3.ForeColor = Color.FromName(offset3TextColor);
+            } catch (Exception) {
+                this.top3.ForeColor = Color.Silver;
+                this.bottom3.ForeColor = Color.Silver;
+            }
+        }
+    }
+
+    public enum Position
+    {
+        Top3,
+        Top2,
+        Top1,
+        Primary,
+        Bottom1,
+        Bottom2,
+        Bottom3
+    }
+}
